8666d0aa8afe77754efd6068feed26c22468856f
Oluwajuwon <oyaludejuwon@yahoo.com>
2024-06-08 21:57:26 +0100

    sql tool now working

--- tools/sql_tool.py
+++ tools/sql_tool.py
@@ -1,19 +1,19 @@
 
-from langchain.agents import initialize_agent
-from langchain.tools import QuerySQLDataBaseTool
+
+
 from langchain_openai import ChatOpenAI
+from langchain.tools import QuerySQLDataBaseTool
 from langchain.sql_database import SQLDatabase
 from dotenv import load_dotenv
 
+
+# Load environment variables
 load_dotenv()
 
-llm = ChatOpenAI(model= 'gpt-3.5-turbo', temperature= 0)
-with SQLDatabase.from_uri('sqlite:///northwind.db') as db:
-    sql_tool = QuerySQLDataBaseTool(db= db, llm= llm)
+# Initialize the language model
+llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=0)
 
+# Initialize the SQL database
+db = SQLDatabase.from_uri('sqlite:///northwind.db')
+sql_tool = QuerySQLDataBaseTool(db=db, llm= llm)
 
-agent = initialize_agent(sql_tool, llm, agent='zero-shot-react-description', verbose=True)
-print(agent.invoke('How many suppliers does northwind have?'))
-
-engine = db.engine
-engine.dispose()
