df714823dd21c042841e1f7ac1aa543611a99615
Oluwajuwon <oyaludejuwon@yahoo.com>
2024-06-08 21:48:23 +0100

    query database tool not malfunctioning

--- tools/sql_tool.py
+++ tools/sql_tool.py
@@ -1,12 +1,19 @@
 
-
-from langchain.tools import QuerySQLDatabaseTool
+from langchain.agents import initialize_agent
+from langchain.tools import QuerySQLDataBaseTool
 from langchain_openai import ChatOpenAI
 from langchain.sql_database import SQLDatabase
+from dotenv import load_dotenv
+
+load_dotenv()
 
 llm = ChatOpenAI(model= 'gpt-3.5-turbo', temperature= 0)
-db = SQLDatabase.from_uri('sqlite:///northwind.db')
+with SQLDatabase.from_uri('sqlite:///northwind.db') as db:
+    sql_tool = QuerySQLDataBaseTool(db= db, llm= llm)
 
-sql_tool = QuerySQLDatabaseTool(db= db, llm= llm)
 
+agent = initialize_agent(sql_tool, llm, agent='zero-shot-react-description', verbose=True)
+print(agent.invoke('How many suppliers does northwind have?'))
 
+engine = db.engine
+engine.dispose()
